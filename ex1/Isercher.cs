5d3c2bbcd1ed89ce5a45326ef2ae354ed4c6214f
Shmuel <sfeld305@gmail.com>
2017-04-06 15:30:25 +0300

    test

--- ex1/Isercher.cs
+++ ex1/Isercher.cs
@@ -9,7 +9,10 @@ namespace ex1
     public interface Isercher<T>
     {
         // the search method
-        Solution<T> search(ISearchable<T> isearchable);
+        Solution<T> search(ISearchable<T> isearchable);/// <summary>
+        /// /////////////
+        /// </summary>
+        /// <returns></returns>
         // get how many nodes were evaluated by the algorithm
         int getNumberOfNodesEvaluated();
     }
